8fddd28ef0c0833e539d8fcebcaacdf5bd021ed5
benghwee <swiftx888@gmail.com>
2023-12-28 07:26:51 +0800

    Add files via upload

---
+++ src/test/java/algorithm/UnitTest01.java
@@ -0,0 +1,27 @@
+package factory;
+
+import java.util.HashMap;
+
+import static junit.framework.TestCase.assertEquals;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
+public class UnitTest01 {
+    
+
+    @BeforeClass
+    public static void instantiate() {
+        
+    }
+
+    @Test
+    public void _01_basic() {
+        
+    }
+}
---
+++ src/test/java/algorithm/UnitTestSuite.java
@@ -0,0 +1,14 @@
+package factory;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+   UnitTest01.class
+})
+
+public class UnitTestSuite {
+
+}
